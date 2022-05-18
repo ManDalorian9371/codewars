fd6e0f78daec7ec4f6f9a0cacc4399eb9de93806
mostafa rafati <mostafarafati888@gmail.com>
2022-05-18 05:32:00 +0430

    44

---
+++ java script challengs/next bigger4kyu.js
@@ -0,0 +1,31 @@
+function nextBigger(n){
+	//your code here
+
+	let newArr = n.toString().split('').map((x) => Number(x));
+	let posi = [];
+	let tot = newArr.reduce((acc, x, i) => acc * (i + 1), 1);
+	let rnd = () => newArr[Math.ceil(Math.random() * newArr.length) - 1];
+	while (posi.length != tot) {
+		let nPosi = Number([ ...Array(newArr.length) ].map((x, i, a) => rnd()).map((x) => x.toString()).join(''));
+		if (posi.indexOf(nPosi) == -1) {
+			let counter = 0;
+			newArr.forEach((x) => {
+				if (nPosi.toString().split('').indexOf(x.toString()) == -1) {
+					counter++;
+				}
+			});
+			if (counter == 0) {
+				posi.push(nPosi);
+			}
+		}
+	}
+
+	posi.sort((a, b) => a - b);
+
+	let res = posi[posi.indexOf(n) + 1];
+	if (res == n) {
+		return -1;
+	} else {
+		return res;
+	}
+}
